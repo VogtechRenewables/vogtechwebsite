d656a3ff8393e7bc88c1edf9be439479f16fcb93
Venura Perera <venura.perera1999@gmail.com>
2024-07-03 11:41:23 -0400

    Added Strapi & Language context to Hgome and Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import backgroundVideo from '../images/waterfall.mp4'; // Update the path to your MP4 video file
 import backgroundImg from '../images/home_windmill.webp';
+import LanguageContext from "../context/LanguageContext";
 
 const Home = () => {
   const backgroundImage1 = require('../images/renewable_transport_img.webp');
@@ -9,8 +10,36 @@ const Home = () => {
   const backgroundImage4 = require('../images/clean_hydro_img.webp');
   const backgroundImage5 = require('../images/green_ammonia_img.webp');
 
+  const { selectedLanguage } = useContext(LanguageContext);
+  const [homeData, setHomeData] = useState(null);
+
+  useEffect(() => {
+    fetchHomeData();
+  }, [selectedLanguage]);
+
+  const fetchHomeData = async () => {
+    if(selectedLanguage === "English"){
+      try {
+        const response = await fetch('http://localhost:1337/api/home');
+        const data = await response.json();
+        setHomeData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching home data:', error);
+      }
+    } else if (selectedLanguage === "French") {
+      try {
+        const response = await fetch('http://localhost:1337/api/home-french');
+        const data = await response.json();
+        setHomeData(data.data.attributes);
+      } catch (error) {
+        console.error('Error fetching home data:', error);
+      }
+    }
+    
+  };
   return (
-    <div>
+    <>
+    {homeData ? <div>
       <div className="relative h-screen">
         <video 
           className="absolute top-0 left-0 object-cover w-full h-full"
@@ -23,41 +52,41 @@ const Home = () => {
         </video>
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
           <h1 className="w-4/5 md:w-2/3 text-white text-center text-3xl md:text-6xl font-bold p-4">
-            Sustainable Fuels, Infinite Journeys: Powering the World Responsibly
+            {homeData.landingText}
           </h1>
         </div>
       </div>
       <div className="flex items-center justify-center h-[16.67vh] bg-[rgba(34,139,34,0.75)] text-white text-center p-4">
         <div>
-          <h3 className="text-xl md:text-3xl font-semibold mb-2">Our Mission</h3>
+          <h3 className="text-xl md:text-3xl font-semibold mb-2">{homeData.landingSubHeader}</h3>
           <h6 className="text-xs md:text-base font-light">
-            To address climate challenges and energy security through rapid development, delivery, and operation of economically rational decarbonized fuels, energy and materials, and decarbonization services.
+            {homeData.landingSubDesc}
           </h6>
         </div>
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center h-auto md:h-[80vh] bg-white text-center p-4">
         <div className="container mx-auto flex flex-col md:flex-row items-center justify-center relative z-10">
           <div className="w-full md:w-2/3 p-6 rounded-lg">
-            <h2 className="text-left text-2xl md:text-3xl font-bold mb-4 text-green-600">Who We Are</h2>
+            <h2 className="text-left text-2xl md:text-3xl font-bold mb-4 text-green-600">{homeData.sectionOneHeader}</h2>
             <p className="text-left text-sm md:text-base text-gray-600 mb-4">
-              Vogtech Renewables is a unique decarbonization enterprise intending to develop and operate multiple synergistic large infrastructure systems that generate climate-positive, stable, and predictable EBITDA in the following global growth sectors:
+              {homeData.paragraph1}            
             </p>
             <ul className="list-disc text-left text-sm md:text-base text-gray-600 mb-4">
               <li>
-                <strong>Renewable Transport Fuels</strong> – Carbon-negative sustainable aviation fuel (SAF), renewable diesel, renewable naphtha, and green hydrogen
+                <strong>{homeData.bulletTitle1}</strong> – {homeData.bulletDesc1}
               </li>
               <li>
-                <strong>Carbon Management</strong> – Development and operation of CO2 capture, pipelines, sequestration sinks, and infrastructure
+                <strong>{homeData.bulletTitle2}</strong> – {homeData.bulletDesc2}
               </li>
               <li>
-                <strong>Carbon Negative Power</strong> – Landfill diverted biomass power
+                <strong>{homeData.bulletTitle3}</strong> – {homeData.bulletDesc3}
               </li>
               <li>
-                <strong>Clean Hydrogen and Hydrogen Derivatives</strong> – Carbon Negative and Climate Positive transportation fuels
+                <strong>{homeData.bulletTitle4}</strong> – {homeData.bulletDesc4}
               </li>
             </ul>
             <p className="text-left text-sm md:text-base text-gray-600 mb-4">
-              Using our proprietary framework that incorporates ESG factors throughout the project lifecycle, Vogtech Renewables optimizes total value and creates a circular economy for current and future generations. Vogtech Renewables' leadership, executive team, and partners bring over 100 years of collective infrastructure experience and have deep experience in the development, delivery, and operations of several billion-dollar large-scale complex infrastructure projects.
+              {homeData.paragraph2}
             </p>
           </div>
           <img
@@ -68,36 +97,40 @@ const Home = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center h-auto md:h-[70vh] bg-[rgba(34,139,34,0.75)] text-white text-center p-4">
-        <h1 className='text-2xl md:text-3xl font-bold mb-4 text-white'>Focus Sectors</h1>
+        <h1 className='text-2xl md:text-3xl font-bold mb-4 text-white'>{homeData.section2Title}</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 w-full md:w-3/4 h-auto md:h-full">
           <div className="transform transition-transform duration-300 hover:scale-105 relative bg-white text-2xl text-white font-black p-4 rounded shadow-lg flex flex-col items-center justify-center text-base md:text-lg font-semibold col-span-1 row-span-1" 
           style={{ backgroundImage: `url(${backgroundImage1})`, backgroundSize: 'cover', height: '200px' }}>
             <div className="absolute inset-0 bg-black opacity-25 rounded"></div>
-            <span className="relative z-10">Renewable Transport Fuels</span>
+            <span className="relative z-10">{homeData.card1}</span>
           </div>
           <div className="transform transition-transform duration-300 hover:scale-105 relative bg-white text-2xl text-white font-black p-4 rounded shadow-lg flex flex-col items-center justify-center text-base md:text-lg font-semibold col-span-1 row-span-1 md:row-span-2" 
           style={{ backgroundImage: `url(${backgroundImage2})`, backgroundSize: 'cover', height: '200px' }}>
             <div className="absolute inset-0 bg-black opacity-25 rounded"></div>
-            <span className="relative z-10">Carbon Management</span>
+            <span className="relative z-10">{homeData.card4}</span>
           </div>
           <div className="transform transition-transform duration-300 hover:scale-105 relative bg-white text-2xl text-white font-black p-4 rounded shadow-lg flex flex-col items-center justify-center text-base md:text-lg font-semibold col-span-1 row-span-1" 
           style={{ backgroundImage: `url(${backgroundImage3})`, backgroundSize: 'cover', height: '200px' }}>
             <div className="absolute inset-0 bg-black opacity-25 rounded"></div>
-            <span className="relative z-10">Carbon Negative Power</span>
+            <span className="relative z-10">{homeData.card3}</span>
           </div>
           <div className="transform transition-transform duration-300 hover:scale-105 relative bg-white text-2xl text-white font-black p-4 rounded shadow-lg flex flex-col items-center justify-center text-base md:text-lg font-semibold col-span-1 row-span-1" 
           style={{ backgroundImage: `url(${backgroundImage4})`, backgroundSize: 'cover', height: '200px' }}>
             <div className="absolute inset-0 bg-black opacity-25 rounded"></div>
-            <span className="relative z-10">Clean Hydrogen and Hydrogen Derivatives</span>
+            <span className="relative z-10">{homeData.card2}</span>
           </div>
           <div className="transform transition-transform duration-300 hover:scale-105 relative bg-white text-2xl text-white font-black p-4 rounded shadow-lg flex flex-col items-center justify-center text-base md:text-lg font-semibold col-span-1 row-span-1" 
           style={{ backgroundImage: `url(${backgroundImage5})`, backgroundSize: 'cover', height: '200px' }}>
             <div className="absolute inset-0 bg-black opacity-25 rounded"></div>
-            <span className="relative z-10">Green Ammonia</span>
+            <span className="relative z-10">{homeData.card5}</span>
           </div>
         </div>
       </div>
-    </div>
+    </div>:
+    
+    <p>Loading...</p>
+    }
+    </>
   );
 }
 
