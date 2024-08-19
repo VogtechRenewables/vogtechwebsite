04e9150942de9bbb1004a10d40d6244fcc432df4
Venura Perera <venura.perera1999@gmail.com>
2024-08-19 16:09:42 -0400

    Fixed email sending from contact form

--- server/server.js
+++ server/server.js
@@ -6,12 +6,15 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5021;
+console.log(process.env.EMAIL_USER)
+console.log(process.env.EMAIL_PASS)
+
 
 const corsOptions = {
   origin: 'http://localhost:3000', // your frontend's URL
   optionsSuccessStatus: 200
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,13 +22,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/send-email', async (req, res) => {
   try {
     const { name, email, message } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      throw new Error('Invalid email address');
+    }
+    
     let transporter = nodemailer.createTransport({
       host: 'mail.vogtech.ca',
       port: 465, // use the correct port for SSL
       secure: true, // true for 465, false for other ports
       auth: {
-        user: process.env.EMAIL_USER, // your corporate email
-        pass: process.env.EMAIL_PASS, // your email account's password
+        user: process.env.EMAIL_USER || "venura@vogtech.ca", // your corporate email
+        pass: process.env.EMAIL_PASS || "_v5+g#OG;HI9", // your email account's password
       },
 
     });
